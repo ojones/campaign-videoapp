6374f595ac26aa0ad26efee9f983420417d6cb42
ojones <wakeupoj@gmail.com>
2017-08-26 00:08:46 -0700

    connect app to featherjs

--- campaign-videos/src/App.js
+++ campaign-videos/src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { socket, feathersClient } from './feathersClient'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { campaigns: [] };
+    const campaigns = feathersClient.service('campaigns');
+    campaigns.find().then( campaignsResonse => {
+      const campaigns = campaignsResonse.data;
+      this.setState({campaigns});
+      console.log(this.state.campaigns)
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +26,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          There are {this.state.campaigns.length} campaigns so far :)
+        </div>
       </div>
     );
   }
